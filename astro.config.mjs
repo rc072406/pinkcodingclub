8e122ba4ca91d8cd1215d947a028ffb97c1da7a2
rc.github.io <rachelcheniphone@gmail.com>
2026-05-20 20:51:48 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://rc072406.github.io/pinkcodingclub/',
-  base: '/pinkcodingclub',
+  site: 'https://pinkcodingclub.pages.dev',
+  base: '/',
 })
